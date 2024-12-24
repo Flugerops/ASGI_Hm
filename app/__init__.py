4a5528b3f8da784d33b87946b7e17328d3c5ab42
Bohdan Savin <bogdan07112009@gmail.com>
2024-12-24 10:56:57 +0200

    small fix

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+# from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+
 from .utils import lifespan
 
 
 app = FastAPI(title=__name__, lifespan=lifespan)
-
+# app.add_middleware(HTTPSRedirectMiddleware) Приклад налаштування безпеки. Потрібен SSL
 
 from . import websockets
